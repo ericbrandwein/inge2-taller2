837f463a2c2b3e8e7ca553c8837a38ae4b0c93b0
Eric Brandwein <brandweineric@gmail.com>
2020-05-13 18:48:27 -0300

    Agrega mvn install a ejecutar.sh

--- src/ejecutar.sh
+++ src/ejecutar.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+JRE=/usr/lib/jvm/java-8-openjdk/jre/lib/rt.jar
+
+mvn install
 javac -target 1.8 -source 1.8 -g examples/A.java
 java -jar \
   zero-analysis/target/zero-analysis-1.0-SNAPSHOT-jar-with-dependencies.jar \
